e2ddf960cecc599597ae1979705817ca52dfce81
Rishab Jha <rishabjha134@gmail.com>
2026-09-17 19:53:13 +0530

    feat: add initial HTML, CSS, and JavaScript files for website layout

---
+++ JAVASCRIPT/chapter1/index.js
@@ -0,0 +1,21 @@
+// const a=5;
+// a=6;
+
+// console.log(a);
+
+let a=5;
+a=11;
+console.log(a)
+
+var b=15;
+b=20;
+
+console.log(b)
+
+// let c=20;
+// let c=50;
+// console.log(c)
+
+var d=20;
+var d=50;
+console.log(d);
